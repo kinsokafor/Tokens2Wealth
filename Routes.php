9c4a5e457dcdcaa6c3b3bc321071fe1262f19cfc
Kowebtech Ict <info@koweb.com.ng>
2023-12-15 15:24:39 +0100

    Update 15/12/23

--- Routes.php
+++ Routes.php
@@ -1,13 +1,13 @@
 <?php  
 
-use Public\Modules\<pluginName>\<pluginPrefix>Controller;
+use Public\Modules\Tokens2Wealth\T2WController;
 use EvoPhp\Api\Requests\Requests;
 
 //API End points
 
 //Pages
 
-$router->get('/<entryURI>', function($params){
-    $controller = new BHController;
-    $controller->{'<entry>/index'}($params)->auth(2,3,4)->setData(["pageTitle" => "Admin"]);
+$router->get('/t2w', function($params){
+    $controller = new T2WController;
+    $controller->{'T2WMain/index'}($params)->auth(2,3,4)->setData(["pageTitle" => "Admin"]);
 }); 
